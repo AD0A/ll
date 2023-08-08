3850ed186fa0c376b35c3af24366830e3f7f2ef4
AD0A <140923972+AD0A@users.noreply.github.com>
2023-08-08 12:52:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let num1 = 7;
+let num2 = 12;
+
+let result = (num1 + num2) * 3;
+console.log(result);
+
+if (result > 50) {
+  console.log("Result is bigger");
+} else {
+  console.log("Result is smaller");
+}
+
+let numberList = [];
+
+for (let i = 0; i <= 5; i++) {
+  numberList.push(i);
+}
+console.log(numberList);
+
+numberList.pop();
+console.log(numberList);
